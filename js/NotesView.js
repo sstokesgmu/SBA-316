cab1ae8f626cd93851c37438253ff96893a0db6b
sstokes <sterlingjstokes@gmail.com>
2024-12-10 21:44:16 -0500

    Added update some functionality to the code, for editing I added a bool flag isDirty to return true if the note title or body has changed; add an event of type change to modify this bool. I added a double click event that will delete the note, eventually. Lastly selection has been modified a add a class to the note list in order to activate the styling

--- js/NotesView.js
+++ js/NotesView.js
@@ -1,13 +1,14 @@
 export default class NotesView {
   //!Will use object deconstruction
   constructor(
-    rootElement, { onNoteSelect, onNoteAdd, onNoteEdit, onNoteDelete } = {}) 
+    rootElement,{onNoteAdd, onNoteDelete, onNoteSelect, onNoteEdit  } = {}) 
     {
         this.rootElement = rootElement;
-        this.onNoteSelect = onNoteSelect;
+        //Events
         this.onNoteAdd = onNoteAdd;
-        this.onNoteEdit = onNoteEdit;
         this.onNoteDelete = onNoteDelete;
+        this.onNoteSelect = onNoteSelect;
+        this.onNoteEdit = onNoteEdit;
 
         //! How come double qoutes will work
         this.rootElement.innerHTML = `   
@@ -17,14 +18,16 @@ export default class NotesView {
             </div>
         </div>
         <div class="notes_editor">
-            <input class="note_title" placeholder="Enter Title ... ">
-            <textarea class="note_body">Write Here ...</textarea>
+            <input class="note_title" placeholder="Enter Title ... " data>
+            <textarea class="note_body" placeholder="Start Writing Here..."></textarea>
         </div>`;
 
         //Right click event to add a new note (Event Listener)
         const btnAddNote = this.rootElement.querySelector(".folder");
         const noteTitle = this.rootElement.querySelector(".note_title");
+        noteTitle.setAttribute("data-isDirty", "false");
         const noteBody = this.rootElement.querySelector(".note_body");
+        noteTitle.setAttribute("data-isDirty", "false");  //! We will use this to check if the value of the title or body text has changed
 
         //! contextMenu will be a substitute for right click
         btnAddNote.addEventListener("contextmenu", () => {
@@ -32,19 +35,28 @@ export default class NotesView {
         });
         
         [noteTitle, noteBody].forEach(inputField => {
-            //! This event happens when we no longer click on the note 
+            //! This event happens when we no longer click on the note or we hit eneter
             inputField.addEventListener("blur", () => {
-                const updatedTitle = noteTitle.value.trim();
-                const updatedBody = noteBody.value.trim();
-
-                this.onNoteEdit(updatedTitle, updatedBody);
-            });
+                if(inputField.dataset.isDirty)
+                    this.updateInputField(inputField)
+                })
+            inputField.addEventListener("change", () => {
+                inputField.dataset.isDirty = true
+            })
         });
+
+        this.setNotePreviewVisible(false);
+    }
+
+    updateInputField(inputField){
+        console.log(`called for ${inputField.className}`);
+        inputField.dataset.isDirty = false;
+        this.onNoteEdit(inputField);
     }
     
     _createListItemHtml(id, title, body, updated) {
         const Max_Body_Length = 40;
-        return `    <section class="notelist-item" data-note-id="${id}">
+        return `<section class="notelist-item" data-note-id="${id}">
                         <section id="note_small_title">${title}</section>
                         <section id="note_small_body">
                             ${body.substring(0,Max_Body_Length)}
@@ -57,25 +69,43 @@ export default class NotesView {
         `;
     }
 
-
     updateNoteList(notes) {
         const notesListContainer = this.rootElement.querySelector(".folder");
-
         //Empty list
         notesListContainer.innerHTML = ""
-    
         for(const note of notes){
             const htmlNode = this._createListItemHtml(note.id, note.title, note.body, new Date(note.last_updated));
-            
             notesListContainer.insertAdjacentHTML("beforeend", htmlNode);
         }
-
         //! Add select/delete events for each list item
-
         notesListContainer.querySelectorAll(".notelist-item").forEach(noteListItem => {
             noteListItem.addEventListener("click", () => {
                 this.onNoteSelect(noteListItem.dataset.noteId); //? What does this mean?
             });
+
+            noteListItem.addEventListener("dblclick", () => {
+                const willDelete = confirm("Do you want to delet this note");
+
+                if (willDelete) 
+                    this.onNoteDelete(noteListItem.dataset.noteId);
+
+            })
         });
+    }
+
+    getActiveNote(note){
+        this.rootElement.querySelector(".note_title").value = note.title;
+        this.rootElement.querySelector(".note_body").value = note.body;
+
+        //? if other elements are already selected then we have to remove it
+        this.rootElement.querySelectorAll(".notelist-item.selected").forEach(noteItem => {
+            noteItem.classList.remove("selected");
+        }) //! Activates the Css selector
+
+        this.rootElement.querySelector(`.notelist-item[data-note-id="${note.id}"]`).classList.add("selected");
+    }
+
+    setNotePreviewVisible(visible){
+        this.rootElement.querySelector(".notes_editor").style.visibility = visible ? "visible" : "hidden";
     }
 }
--- js/main.js
+++ js/main.js
@@ -2,20 +2,28 @@ import NotesView from "./NotesView.js"
 import NotesAPI from "./NotesAPI.js"
 
 const app = document.getElementById("app");
-console.log(app);
 const view = new NotesView(app, {
-    
-    onNoteSelect(id){
-        console.log(`Note selected: ${id}`);
-    },
+    //?Does the order matter
+ 
     onNoteAdd() { 
         console.log("Note has been added!");
     },
-    onNoteEdit(newTitle, newBody){
-        console.log(newTitle);
-        console.log(newBody);
+    onNoteDelete(id){
+        console.log(`Note Deleted: ${id}`);
     },
-    
+    onNoteSelect(id){
+        console.log(`Note selected: ${id}`);
+        let notes = NotesAPI.getAllNotes();
+        console.log(notes);
+        view.getActiveNote(notes[1]);
+       // view.getActiveNote();
+    },
+    onNoteEdit(inputField){
+        console.log("Open note has been edited")
+        // console.log(newTitle);
+        // console.log(newBody);
+    },
 });
 
 view.updateNoteList(NotesAPI.getAllNotes());
+
